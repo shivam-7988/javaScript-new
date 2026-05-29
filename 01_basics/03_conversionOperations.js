2d928af8768f8d9c4037da6de6782cf49c96cfeb
Shivam <shivamhass07@gmail.com>
2026-05-29 15:48:09 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanLoggedIn)
+
+
+
+
