ae24c48de4a94d192956fdba86f56015fdddbac7
Shivam <shivamhass07@gmail.com>
2026-05-29 15:22:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144232    
+let accountEmail = "shivamtest@gmail.com"
+var accountPassword = "1431245"
+accountCity = "Gurgaon"
+let accountState;
+
+// accountId = 2 // Not allowed
+
+accountEmail = "nayi@gmail.com"
+
+accountPassword = "4290000"
+
+accountCity = "Jaipur"
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+// This is all about Variables
+
+
+
