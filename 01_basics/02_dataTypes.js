7da1da0ec670f0b4a4b455c952321b78f06bc918
Shivam <shivamhass07@gmail.com>
2026-05-29 11:56:34 +0000

    datatyes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // We are using nodejs, not browser
+
+console.log(3+3)
+console.log("Shivam")
+
+let name = "Shivam"
+let age = 19
+let isLoggedIn = false 
+
+// number 
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // Output => undefined
+console.log(typeof null) // Output => object
+
