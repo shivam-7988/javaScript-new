07c4db5d88172b1d0351286f299ee7e08a14028e
Shivam <shivamhass07@gmail.com>
2026-05-30 12:20:18 +0000

    conversion

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,24 @@
+console.log(null > 0)  // false
+console.log(null == 0); // false
+console.log(null < 0);  // false
+console.log(null <= 0); // true
+console.log(null >= 0); // true
+
+
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+console.log(undefined == 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false // strict check 
+
+
+
+
+
+
+
+
