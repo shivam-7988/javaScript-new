9ebc96cffcbf1bf615652f8f7ec043e6c7923938
Shivam <shivamhass07@gmail.com>
2026-06-02 16:04:17 +0000

    splice summary

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -16,18 +16,24 @@ console.log(myArr[1]);
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(5));
 
-const newArr = myArr.join()
-console.log(myArr);
+// const newArr = myArr.join()
+// console.log(myArr);
 
-console.log(typeof(newArr));
+// console.log(typeof(newArr));
 
 // ------------------Slice, Splice---------------------------
 
-console.log("A" , myArr);
-const myn1 = myArr.slice(1,3)
+// console.log("A" , myArr);
+// const myn1 = myArr.slice(1,3)
 
-console.log(myn1);
-console.log("B" , myArr);
+// console.log(myn1);
+// console.log("B" , myArr);
+
+const myArr4 = [0,1,2,3,4,5,6]
+console.log("C" , myArr4.splice(1,4));
+console.log(myArr4);
+
+
 
 
 
