a4aa3235c20743c8312dc1bf5d28e369297d87de
Shivam <shivamhass07@gmail.com>
2026-06-06 14:34:32 +0000

    updating array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -50,6 +50,13 @@ console.log(Array.isArray("Shivam"))
 console.log(Array.from("Shivam"))
 console.log(Array.from({name: "hitesh"})) // interesting
 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
 
 
 
