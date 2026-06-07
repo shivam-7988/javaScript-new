17d1ac8a29ad3a610cfed5cf94dc9082434a1819
Shivam <shivamhass07@gmail.com>
2026-06-07 16:22:45 +0000

    objects summary

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -3,9 +3,16 @@
 // object literals
 
 //Object.create 
+
+const mySym = Symbol("Key1")
+
+
+
+
 const JsUser = {
     name: "Shivam",
     "full Name": "Shivam Hans",
+    [mySym] : "myKey1",
     age: 18,
     location: "jaipur",
     email: "shivam@gmail.com",
@@ -16,4 +23,23 @@ const JsUser = {
 console.log(JsUser.email);
 console.log(JsUser["email"]);
 console.log(JsUser["full Name"]);
+console.log(typeof JsUser[mySym]);
+
+JsUser.email = "newmail@gmail.com"
+// Object.freeze(JsUser)  // after this values can not change
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
 
